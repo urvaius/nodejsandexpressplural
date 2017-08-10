fd77dbb81ffa35ec69153798d86e0f1495432d44
Dave Arneson <urvaius@gmail.com>
2017-08-10 15:01:19 -0700

    lint section complete L0

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,7 +3,7 @@ import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
-
+/* eslint-disable no-console */
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
