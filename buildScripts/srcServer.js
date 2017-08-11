8ca1ad52912dd738042b2dbbe2946e4103600c59
Dave Arneson <urvaius@gmail.com>
2017-08-11 11:13:50 -0700

    work inprogress on the demo html app part

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,15 @@ app.get('/',function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // hard coding for simplicity and testing should go to database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 2,"firstName":"Tina","lastName":"Lee","email":"leetina@yahoo.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
