4b5a119cb49bac24e71c69b734d95665f329184a
Dave Arneson <urvaius@gmail.com>
2017-08-11 12:29:13 -0700

    finished making http calls. next is project structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 import './index.css';
 
 // Populate table of users via API call.
@@ -17,4 +17,18 @@ getUsers().then(result => {
 
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // must use array. form to create a reql array from a dom collection
+  // get elementsbyclassname only returns an array like object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
