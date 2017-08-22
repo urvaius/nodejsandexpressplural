0724ffb97ad7c33cf1a7cde12b7a59da40928f29
Dave Arneson <urvaius@gmail.com>
2017-08-22 12:59:32 -0700

    did bundliing etc. next module is production deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,22 +1,34 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMD5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    // generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    // Hash the files using md5 so that their names change when content changes.
+    new WebpackMD5Hash(),
+    // use commonschunkplugin to create a seperate bundle
+    // of vendor libraries so that they're cached seperately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     // create html file that includes reference to bundled js.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
@@ -32,7 +44,10 @@ export default {
       //  minifyCSS: true,
       //  minifyURLs: true
     //  },
-      inject: true
+      inject: true,
+      // properties you define here are available in index.html
+      //using htmlwebpackplugin.options.varname
+      trackJSToken:'e24adc9a278f42a2baac04abf0cff77e'
     }),
     // eliminate duplicate package when generating bundle
     new webpack.optimize.DedupePlugin(),
@@ -42,7 +57,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style', 'css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
